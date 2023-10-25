b5783e7f1f37cfa417a37458d46145cffde2a175
Andre Alves <andrealves10a@gmail.com>
2023-10-25 16:47:48 -0300

    feat: formating code

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketLine: false,
+  printWidth: 80,
+};
+
+export default config;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./pages/home/home.component";
-import { CartComponent } from "./pages/cart/cart.component";
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { CartComponent } from './pages/cart/cart.component'
 
 const routes: Routes = [
   {
-    path: "home",
+    path: 'home',
     component: HomeComponent,
   },
   {
-    path: "cart",
+    path: 'cart',
     component: CartComponent,
   },
   {
-    path: "",
-    redirectTo: "home",
-    pathMatch: "full",
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
