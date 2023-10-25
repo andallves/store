ea201e0bfa1062105a3e80583b1db8fef9bf92a9
Andre Alves <andrealves10a@gmail.com>
2023-10-25 16:50:37 -0300

    feat: implement filter functionality

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,15 +1,38 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
-  selector: "app-filters",
-  templateUrl: "./filters.component.html",
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCategory = new EventEmitter<string>();
+  categoriesSubscription: Subscription | undefined;
+  categories: Array<string> | undefined;
 
-  categories = ["shoes", "sports"];
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService
+      .getAllCategories()
+      .subscribe((_categories) => (this.categories = _categories));
+  }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
