9da6f88da57a0ce6d143889786a12b62d555b9ae
Andre Alves <andrealves10a@gmail.com>
2023-10-25 16:50:04 -0300

    feat: add data and functionality

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { Cart, CartItem } from "src/app/models/cart.model";
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
-  selector: "app-cart",
-  templateUrl: "./cart.component.html",
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
 })
 export class CartComponent implements OnInit {
   cart: Cart = {
     items: [
       {
-        product: "https://via.placeholder.com/150",
-        name: "snickers",
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
         price: 150,
         quantity: 2,
         id: 1,
       },
       {
-        product: "https://via.placeholder.com/150",
-        name: "snickers",
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
         price: 120,
         quantity: 4,
         id: 2,
@@ -27,21 +28,40 @@ export class CartComponent implements OnInit {
 
   dataSource: Array<CartItem> = [];
   displayedColumns: Array<string> = [
-    "product",
-    "name",
-    "price",
-    "quantity",
-    "total",
-    "action",
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
   ];
 
+  constructor(private cartService: CartService) {}
+
   ngOnInit(): void {
-    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items
-      .map((item) => item.price * item.quantity)
-      .reduce((prev, current) => prev + current, 0);
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveItem(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
   }
 }
